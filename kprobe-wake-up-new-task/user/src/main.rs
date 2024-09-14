50e9e48a2881c1f47c1b08dcc2827a8e30488ca0
yukinakanaka <yuki.nakamura@mapbox.com>
2024-09-14 15:49:50 +0900

    feat: add kprobe-wake-up-new-task

--- kprobe-wake-up-new-task/user/src/main.rs
+++ kprobe-wake-up-new-task/user/src/main.rs
@@ -1,5 +1,5 @@
+use aya::programs::KProbe;
 use aya::{include_bytes_aligned, Bpf};
-use aya::{programs::BtfTracePoint, Btf};
 use aya_log::BpfLogger;
 
 use std::convert::TryInto;
@@ -32,23 +32,20 @@ async fn main() -> Result<(), anyhow::Error> {
     // This will include your eBPF object file as raw bytes at compile-time and load it at runtime.
     #[cfg(debug_assertions)]
     let mut bpf = Bpf::load(include_bytes_aligned!(
-        "../../target/bpfel-unknown-none/debug/raw_trace_sched_process_exec"
+        "../../target/bpfel-unknown-none/debug/kprobe_wake_up_new_task"
     ))?;
     #[cfg(not(debug_assertions))]
     let mut bpf = Bpf::load(include_bytes_aligned!(
-        "../../target/bpfel-unknown-none/release/raw_trace_sched_process_exec"
+        "../../target/bpfel-unknown-none/release/kprobe_wake_up_new_task"
     ))?;
     if let Err(e) = BpfLogger::init(&mut bpf) {
         // This can happen if you remove all log statements from your eBPF program.
         warn!("failed to initialize eBPF logger: {}", e);
     }
-    let btf = Btf::from_sys_fs()?;
-    let program: &mut BtfTracePoint = bpf
-        .program_mut("raw_trace_sched_process_exec")
-        .unwrap()
-        .try_into()?;
-    program.load("sched_process_exec", &btf)?;
-    program.attach()?;
+
+    let program: &mut KProbe = bpf.program_mut("wake_up_new_task").unwrap().try_into()?;
+    program.load()?;
+    program.attach("wake_up_new_task", 0)?;
 
     info!("Waiting for Ctrl-C...");
     signal::ctrl_c().await?;
--- kprobe-wake-up-new-task/xtask/src/codegen.rs
+++ kprobe-wake-up-new-task/xtask/src/codegen.rs
@@ -3,7 +3,7 @@ use std::{fs::File, io::Write, path::PathBuf};
 
 pub fn generate() -> Result<(), anyhow::Error> {
     let dir = PathBuf::from("ebpf/src");
-    let names = vec!["trace_event_raw_sched_process_exec", "task_struct"];
+    let names = vec!["task_struct"];
     let bindings = aya_tool::generate(
         InputFile::Btf(PathBuf::from("/sys/kernel/btf/vmlinux")),
         &names,
